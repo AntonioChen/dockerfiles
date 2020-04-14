22457a7018581d0508b3b8f3651c3d3c33d45288
Antonio Chen <chenxuanyi1991@gmail.com>
2020-04-14 16:16:13 +0800

    1

--- buuctf_2018_online_tool/files/html/index.php
+++ buuctf_2018_online_tool/files/html/index.php
@@ -1,18 +1,12 @@
 <?php
 
-if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-    $_SERVER['REMOTE_ADDR'] = $_SERVER['HTTP_X_FORWARDED_FOR'];
-}
-
 if(!isset($_GET['host'])) {
     highlight_file(__FILE__);
 } else {
     $host = $_GET['host'];
-    $host = escapeshellarg($host);
-    $host = escapeshellcmd($host);
     $sandbox = md5("glzjin". $_SERVER['REMOTE_ADDR']);
     echo 'you are in sandbox '.$sandbox;
     @mkdir($sandbox);
     chdir($sandbox);
-    echo system("nmap -T5 -sT -Pn --host-timeout 2 -F ".$host);
+    echo system("ping -c 5 ".$host);
 }
